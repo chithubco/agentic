7f3baa0d25aefb042a08b177b7ce0402e2efef16
Sirmikky <michael.agbogo@echithub.com>
2025-03-10 23:08:55 +0100

    Add initial application structure with configuration and UI components

--- src/app/ui/uiconfigfile.py
+++ src/app/ui/uiconfigfile.py
@@ -1,18 +1,19 @@
 from configparser import ConfigParser
 
+
 class Config:
-    def __init__(self,config_file="./src/app/ui/config.ini"):
+    def __init__(self, config_file="./src/app/ui/uiconfigfile.ini"):
         self.config = ConfigParser()
         self.config.read(config_file)
 
     def get_llm_options(self):
-        return self.config['DEFAULT'].get('LLM_OPTIONS').split(",")
+        return self.config["DEFAULT"].get("LLM_OPTIONS").split(", ")
 
-    def get_groq_options(self):
-        return self.config['DEFAULT'].get('GROQ_MODEL_OPTIONS').split(",")
+    def get_usecase_options(self):
+        return self.config["DEFAULT"].get("USECASE_OPTIONS").split(", ")
 
-    def get_use_case_options(self):
-        return self.config['DEFAULT'].get('USE_CASE_OPTIONS').split(",")
+    def get_groq_model_options(self):
+        return self.config["DEFAULT"].get("GROQ_MODEL_OPTIONS").split(", ")
 
-    def get_page_title_options(self):
-        return self.config['DEFAULT'].get('PAGE_TITLE').split(",")
+    def get_page_title(self):
+        return self.config["DEFAULT"].get("PAGE_TITLE")
